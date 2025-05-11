59161cb30577738075a3638ce571a3eb4e3ab193
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-05-11 18:27:47 +0300

    feat: add SkillsSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import AboutMe from "../components/AboutMe";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
+import SkillsSection from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -17,6 +18,7 @@ export const Home = () => {
       <main>
         <HeroSection />
         <AboutMe />
+        <SkillsSection />
       </main>
       {/* Footer */}
     </div>
